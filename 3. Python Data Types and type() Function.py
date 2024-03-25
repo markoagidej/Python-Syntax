9ebf2abef8dd5b7b6e4fe16038c16a6fc49b87c9
Marko <markogidej@yahoo.com>
2024-03-25 12:05:51 -0400

    Answered Question 3 all tasks

---
+++ 3. Python Data Types and type() Function.py
@@ -0,0 +1,11 @@
+# Task 1
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print("Type of variable_a is: " + str(type(variable_a)))
+print("Type of variable_b is: " + str(type(variable_b)))
+print("Type of variable_c is: " + str(type(variable_c)))
+print("Type of variable_d is: " + str(type(variable_d)))
