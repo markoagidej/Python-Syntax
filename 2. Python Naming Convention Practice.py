9c24eab39753a0df9aaf34778215b891fd96e5e6
Marko <markogidej@yahoo.com>
2024-03-25 11:58:37 -0400

    Answered Question 2 all tasks

---
+++ 2. Python Naming Convention Practice.py
@@ -0,0 +1,11 @@
+# Task 1
+
+
+PI_VALUE = 3.14
+
+# There was never a distinction made when to use snake_case vs camelCase for naming variables (also ambiguity as to what camelCase is), therefore I conclude both of these are "valid". However it is recommended to be consistent with a naming convention.
+# After searching online, it seems like snake_case is the more common variable naming convention for python, so that is what I chose here.
+user_age = 25
+user_location = "New York"
+
+MAX_LIMIT = 1000
