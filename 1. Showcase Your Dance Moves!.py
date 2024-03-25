4909f6f0193e26c8355ea3bb16f7581bac278983
Marko <markogidej@yahoo.com>
2024-03-25 11:28:35 -0400

    Converted all files to Python and answered question 1 all tasks

---
+++ 1. Showcase Your Dance Moves!.py
@@ -0,0 +1,21 @@
+# Task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+# Task 2
+    
+print("How do you feel today?")
+response = input()
+
+if response == "happy":
+    print("That's great to hear!")
+elif response == "sad":
+    print("I hope your day gets better!")
+else:
+    print("I don't quite understand!")
